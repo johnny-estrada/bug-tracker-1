c24147efa7dd8e32d1d2f40143913ad8c308d997
Johnny Estrada <johnnye562@yahoo.com>
2023-02-03 22:11:31 -0800

    Initial commit

---
+++ server/index.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const database = require("./models");
+
+app.use(cors());
+app.use(express.json());
+
+app.listen(5000, () => {
+  console.log("Server is running on port 5000...");
+
+  database.sequelize.sync().then(() => {
+    console.log("Database connection successful...");
+  });
+});
