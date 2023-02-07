9673ceecdf1e8a3c883b671421538fdaad942627
Johnny Estrada <johnnye562@yahoo.com>
2023-02-06 18:43:56 -0800

    Add projectRouter to index.js/server file

--- server/index.js
+++ server/index.js
@@ -6,6 +6,11 @@ const database = require("./models");
 app.use(cors());
 app.use(express.json());
 
+// Routers
+const projectRouter = require("./routes/projectRouter");
+
+app.use("/project", projectRouter);
+
 app.listen(5000, () => {
   console.log("Server is running on port 5000...");
 
