2d12bb32e834dbad77ce7ec48d4ef23c173396e7
Johnny Estrada <johnnye562@yahoo.com>
2023-02-06 18:42:15 -0800

    Create projectController for list and create

---
+++ server/controllers/projectController.js
@@ -0,0 +1,21 @@
+const { Project } = require("../models");
+
+module.exports = {
+  listAction: async function (req, res) {
+    try {
+      const listOfProjects = await Project.findAll()
+      res.json(listOfProjects)
+    } catch (error) {
+      console.log(error);
+    }
+  },
+  createAction: async function (req, res) {
+    try {
+      const project = req.body;
+      await Project.create(project);
+      res.json(project)
+    } catch (error) {
+      console.log(error);
+    }
+  },
+};
