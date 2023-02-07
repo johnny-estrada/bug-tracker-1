ac8a69fdf635421f4fa6ad92e0140dca49f5b684
Johnny Estrada <johnnye562@yahoo.com>
2023-02-06 18:41:20 -0800

    Create Project model

---
+++ server/models/Project.js
@@ -0,0 +1,18 @@
+module.exports = (sequelize, DataTypes) => {
+  const Project = sequelize.define("Project", {
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    key: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    description: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+  });
+
+  return Project;
+};
