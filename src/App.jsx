7e977f77ff5187a741ce6a779dc9b2b6e9b24740
Johnny Estrada <johnnye562@yahoo.com>
2023-02-14 23:10:30 -0800

    Update App component and minor edit to styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
-import { Routes, Route} from "react-router-dom";
-import Navbar from "./components/Navbar";
-import Sidebar from "./components/Sidebar";
+import { Routes, Route } from "react-router-dom";
+import Layout from "./layouts/Layout";
 import Home from "./pages/Home";
 import Projects from "./pages/Projects";
 import "./App.css";
@@ -8,15 +7,12 @@ import "./App.css";
 function App() {
   return (
     <div className="container">
-      {/* <Navbar /> */}
-      <Sidebar />
-      
-      <main className="main">
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/projects" element={<Projects />} />
-        </Routes>
-      </main>
+      <Layout>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/projects" element={<Projects />} />
+          </Routes>
+      </Layout>
     </div>
   );
 }
