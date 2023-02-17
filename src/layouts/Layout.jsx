3603967758ef522898b40ee9680ad1d781051f4d
Johnny Estrada <johnnye562@yahoo.com>
2023-02-16 23:04:28 -0800

    Update Layout class names

--- src/layouts/Layout.jsx
+++ src/layouts/Layout.jsx
@@ -1,21 +1,19 @@
-import Sidebar from "../components/Sidebar";
-import TopNavbar from "../components/TopNavbar";
-import "./Layout.css";
+import Sidebar from "./Sidebar";
+import Navbar from "./Navbar";
+
+import "../assets/styles/Layout.css";
 
 const Layout = ({ children }) => {
   return (
-    <>
-      <div class="layout-container">
-        <div class="left-navbar">
-          <Sidebar />
-        </div>
-        <div class="main-content">
-          <TopNavbar />
-
-          <main>{children}</main>
-        </div>
+    <div class="container">
+      <div class="sidebar">
+        <Sidebar />
       </div>
-    </>
+      <div class="content">
+        <Navbar />
+        <main className="main">{children}</main>
+      </div>
+    </div>
   );
 };
 
