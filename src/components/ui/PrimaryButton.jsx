cb4c1213dede3af24fe9dee6841c5ee0387f32fb
Johnny Estrada <johnnye562@yahoo.com>
2023-02-21 18:18:41 -0800

    Create PrimaryButton component and styles

---
+++ src/components/ui/PrimaryButton.jsx
@@ -0,0 +1,7 @@
+import "../../assets/styles/PrimaryButton.css";
+
+function PrimaryButton() {
+  return <button className="primary-button">PrimaryButton</button>;
+}
+
+export default PrimaryButton;
