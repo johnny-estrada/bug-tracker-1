830ad7ff7b13c1e3823b7df2b7f887b7e8f72ada
Johnny Estrada <johnnye562@yahoo.com>
2023-02-21 17:57:09 -0800

    Add new page routes to App

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,22 @@ import { Routes, Route } from "react-router-dom";
 import Layout from "./layouts/Layout";
 import Home from "./pages/Home";
 import Projects from "./pages/Projects";
-import "./App.css";
+import Issues from "./pages/Issues";
+import Roles from "./pages/Roles";
+import Users from "./pages/Users";
+import "./assets/styles/App.css";
 
 function App() {
   return (
-    <div className="container">
+    <div className="App">
       <Layout>
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/projects" element={<Projects />} />
-          </Routes>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/issues" element={<Issues />} />
+          <Route path="/roles" element={<Roles />} />
+          <Route path="/users" element={<Users />} />
+        </Routes>
       </Layout>
     </div>
   );
