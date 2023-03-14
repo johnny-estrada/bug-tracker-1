3b49f765a607812055d2451a0024573da4b69c3f
Johnny Estrada <johnnye562@yahoo.com>
2023-03-13 20:57:07 -0700

    Remove test css class and styles from App

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,13 @@
+import React from "react";
 import SplitPane from "./layouts/SplitPane";
 import Sidebar from "./layouts/Sidebar";
 import Navbar from "./layouts/Navbar";
 import Main from "./layouts/Main";
-
 import Searchbar from "./components/forms/Searchbar";
-
 import Logo from "./components/ui/Logo";
 import Links from "./components/ui/Links";
 import Avatar from "./components/ui/Avatar";
 import NotificationIcon from "./components/ui/NotificationIcon";
-// import Notification from "./components/ui/Notification";
 
 import "./App.css";
 
